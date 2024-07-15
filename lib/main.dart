3b9814b04b6dd1791a2b8655dbf868a03cada90e
Arinda Alvin Aaron <alvinaaronarinda@gmail.com>
2024-07-15 11:48:01 +0300

    Fourth Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   // This widget is the root of your application.
